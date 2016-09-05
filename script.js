3c28181b5597185f9825d99da3a0ecf5c18f9c13
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2016-09-05 15:54:55 -0400

    add ability to flip through image frames

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 
 $(document).ready(function(){
+
+    $("#left-btn").click(function(e) {
+        e.stopPropagation();
+        img = document.getElementById("frame");
+        frameNum = parseInt(img.getAttribute("data").slice(1));
+
+        // TODO: make this less hacky
+        if (frameNum == 0) {
+            newFrameNum = 14;
+            console.log("here")
+        } else {
+            newFrameNum = frameNum - 1;
+            console.log("nope")
+        }
+
+        img.src = "images/frame" + newFrameNum + ".jpg";
+        img.setAttribute("data", "f" + newFrameNum);
+    });
+
+    $("#right-btn").click(function(e) {
+        e.stopPropagation();
+        img = document.getElementById("frame");
+        frameNum = parseInt(img.getAttribute("data").slice(1));
+        newFrameNum = (frameNum + 1) % 15;
+        console.log(newFrameNum);
+        img.src = "images/frame" + newFrameNum + ".jpg";
+        img.setAttribute("data", "f" + newFrameNum);
+    });
+    
     $(".project").click(function() {
         details = $(this).find(".more-details");
         details.toggle();
         bgcolor = details.is(":visible") ? "#eee" : "#fff";
         $(this).css("background-color", bgcolor);
     });
-});
 
+
+});
 
